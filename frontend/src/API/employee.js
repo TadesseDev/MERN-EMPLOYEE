b8010900c3b400c97c7fb125eee6adb1fc8a813f
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-23 16:52:32 +0300

    Get and set user done

--- frontend/src/API/employee.js
+++ frontend/src/API/employee.js
@@ -1,12 +1,19 @@
 const createURL = `http://localhost:4000/employees/create`;
-
+const getUsersURL = `http://localhost:4000/employees/list`
 const createEmployee = (user) => {
   fetch(createURL, {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(user)
-  });
+  }).then(response => console.log(response))
+    .catch(error => console.error(`fail to add user :${error}`))
   console.log(JSON.stringify(user));
 }
 
-export { createEmployee }
+const getEmployees = () => {
+  fetch(getUsersURL)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(`fail to fetch :${error}`));
+}
+export { createEmployee, getEmployees }
--- frontend/src/pages/ListEmployee.jsx
+++ frontend/src/pages/ListEmployee.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import {getEmployees} from '../API/employee'
 export default function ListUser() {
   return (
-    <div>List users</div>
+    <div>
+      <button type="submit"
+        onClick={getEmployees}>list users</button>
+    </div>
   )
 }
