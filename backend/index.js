df2ab893aeec786ca02c03cdda2e40446bdcdb3f
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-23 15:39:38 +0300

    Configure dotenv variable

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const connectDB = require('./models/db')
-const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-app.listen(3001);
+const app = express();
+app.listen(3001, () => console.log('server running'));
 
 app.use(express.urlencoded())
 app.use(express.json())
 app.use(cors());
 
 connectDB();
+const db = mongoose.connection
+db.on('error', err => console.log(err));
+db.once('open', () => console.log('connection success'));
 app.get('/', (req, res) => {
   res.send('hitting the root server');
 });
--- backend/models/db.js
+++ backend/models/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    const conn = await
-      mongoose.connect(`mongodb+srv://tadesse:123@cluster0.51fwj.mongodb.net/?retryWrites=true&w=majority`, {})
-    console.log(`connection successfull ${conn}`);
+    await
+      mongoose.connect(process.env.MANGO_URI, {})
   } catch (error) {
     console.log(`fail`);
   }
 }
+
 module.exports = connectDB;
