1d5cf982224740ed667da9f70b47647257110235
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-29 21:30:03 +0300

    Add a docker config for a server

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const connectDB = async () => {
+  // console.log("connecting to", process.env.MANGO_URI_LOCAL);
   try {
     await
-      mongoose.connect(process.env.MANGO_URI, {})
+      mongoose.connect(process.env.MANGO_URI_LOCAL, {})
   } catch (error) {
     console.log(`fail`);
   }
